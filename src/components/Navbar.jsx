a034645a8106f62f564426af651b57e1969a50a1
Felix Toledo <felixtoledoctes@gmail.com>
2023-02-17 13:00:04 -0300

    navbar design started

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,30 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => (
   <nav className="navbar">
-      <Link className="navbar-brand" to="/">Inicio</Link>
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link className="nav-link" to="/products">Productos</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/about">Sobre Nosotros</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/contact">Contacto</Link>
-        </li>
-      </ul>
-  </nav>
+  <div className="navbar-bottom">
+  <Link className="navbar-brand" to="/">Inicio</Link>
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <Link className="nav-link" to="/products">Productos</Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/about">Sobre Nosotros</Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/contact">Contacto</Link>
+      </li>
+    </ul>
+  </div>
+  <div className="navbar-top">
+    <div className="social-media">
+    <p>aca van las redes</p>
+    </div>
+    <h1>CENTENARIO</h1>
+    <div className="costumer-info">
+    <p>aca va el carrito</p>
+    </div>
+  </div>
+</nav>
 );
 
 export default Navbar;
